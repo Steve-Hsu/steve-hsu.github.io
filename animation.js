c0cf96e1d7d3cb3c8f7ea76fcfaf83dde0145e00
Steve-Hsu <wuujdsc@gmail.com>
2019-08-30 13:17:50 +0800

    2018 resume QC dept

---
+++ animation.js
@@ -0,0 +1,11 @@
+// const anime = require("anime.js");
+
+anime({
+  targets: '.circle',
+  translateX: 200,
+  direction:'alternate',
+  loop: true,
+  background:'rgb(210, 55, 45)',
+  // delay: 100,
+  easing:'easeInOutCirc'
+});
