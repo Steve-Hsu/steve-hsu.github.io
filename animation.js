517a507255a337f4611c378ad689549bbc3df09e
Steve-Hsu <wuujdsc@gmail.com>
2019-09-05 13:02:24 +0800

    Add KaiLong

--- animation.js
+++ animation.js
@@ -1,88 +1,100 @@
 // jshint esversion:6
-var eeee =  document.querySelector("#card").clientWidth;
+var eeee = document.querySelector("#card").clientWidth;
 var sw = document.body.clientWidth;
 var gap = document.querySelectorAll(".triGap");
 var num = 0;
-var protectSwitch = true;
-var triSwicth = false;
-console.log(gap);
+var loopFinish = 0;
+var loopFinishReverse = 0;
+var switchBar = true;
+
+
 var rect = document.querySelector(".L6").getBoundingClientRect();
 console.log(rect.top, rect.right, rect.bottom, rect.left);
 
-function ranDom(){
+function ranDom() {
   var n = Math.random();
   n = n * 2;
   n = Math.floor(n);
   return n;
 }
 
-function B (){
-  gap.forEach(function(x){
-    var num = ranDom();
-    if (num == 1 ){
-      x.classList.add("triGapBlack");
-    } else {
-      x.classList.remove("triGapBlack");
 
-    }
-    console.log("yes");
-  });
-}
+document.querySelector("#card").onclick = function(){
+  console.log('use this '+switchBar);
+  change(switchBar);
+  switchBar = !switchBar;
 
-if (protectSwitch === true){
-  document.querySelector('#card').onclick =function(){
-    triSwicth = !triSwicth;
-    eeee =  document.querySelector("#card").clientWidth;
-  if (triSwicth === true){
-      num = ranDom();
-      return eS.play();
-  } else {
-      return eS.reverse();
+};
+
+function change(x){
+  switch(x){
+    case(false):
+    eS.reverse();
+    break;
+
+    default:
+    eS.play();
   }
-
-  };
-} else {
-  console.log('anime is runing');
 }
 
+// Code if to warp the onclick will have problem !!!! don't delete it until you note it!!
+// if (loopSwich === 'true'){
+//   document.querySelector("#card").onclick = function(){
+// console.log(loopSwich+" is true");
+//  loopSwich = !loopSwich;
+//   };
+// } else {
+//   document.querySelector("#card").onclick = function(){
+//     console.log(loopSwich+" is false");
+//  loopSwich = !loopSwich;
+//   };
+// }
 
 
-// document.querySelector('.tri').onclick =function(){
-// eeee =  document.querySelector("#card").clientWidth;
-//   return eS.reverse();
-// };
-
-  var eS = anime.timeline({
+var eS = anime.timeline({
     // targets: '.tri',
     loop: 1,
     // easing: 'easeInOutSine',
     easing: 'easeOutElastic(1, .8)',
     autoplay: false,
-    update: function(){
-      protectSwitch = false;
-    }
   })
   .add({
     targets: '.tri',
     background: 'rgb(0, 0, 0)',
-    translateX: eeee * 0.5 - (88+24),
+    translateX: eeee * 0.5 - (88 + 24),
     width: '20px',
+    loopComplete: function(){
+      loopFinish = loopFinish + 1;
+      loopFinishReverse +=1;
+    },
   })
-  .add
-  ({
+  .add({
     targets: '.tri',
     width: '176px',
     borderRadius: '0%',
+    loopComplete:  function(){
+      loopFinish = loopFinish + 1;
+      loopFinishReverse +=1;
+    },
   })
   .add({
     targets: '.triGap',
     background: 'rgb(255, 255, 255)',
-    easing:'easeInQuad',
-    width: function(){ if(ranDom() > 0) return 20;},
-    height: function(){ if(ranDom() > 0) return 20;},
+    easing: 'easeInQuad',
+    width: function() {
+      if (ranDom() > 0) return 20;
+    },
+    height: function() {
+      if (ranDom() > 0) return 20;
+    },
     duration: 200,
-    // function(){ if(ranDom() == 0) return 20; },
-    complete:function(){
-        protectSwitch = true;
-      }
+    loopComplete:  function(){
+      loopFinish = loopFinish + 1;
+      loopFinishReverse +=1;
+      console.log('loopFinish is: '+loopFinish);
+
+
+    },
+
   });
+// document.querySelector("#card").onclick = eS.play;
