297c5b2832af00fc06d121506bc7eb1fc78617d1
Steve-Hsu <wuujdsc@gmail.com>
2019-09-10 09:56:24 +0800

    fix home page's trigrams

--- animation.js
+++ animation.js
@@ -1,25 +1,23 @@
 // jshint esversion:6
-var eeee = document.querySelector("#card").clientWidth;
+
 var sw = document.body.clientWidth;
+var hCardWidth = document.querySelector("#HCard").clientWidth;
 var gap = document.querySelectorAll(".triGap");
-var num = 0;
-var loopFinish = 0;
-var loopFinishReverse = 0;
 var switchBar = true;
-
-
 var rect = document.querySelector(".L6").getBoundingClientRect();
 console.log(rect.top, rect.right, rect.bottom, rect.left);
+
 
 function ranDom() {
   var n = Math.random();
   n = n * 2;
   n = Math.floor(n);
+  // console.log("the random number is "+n);
   return n;
 }
 
 
-document.querySelector("#card").onclick = function(){
+document.querySelector("#HCard").onclick = function(){
   console.log('use this '+switchBar);
   change(switchBar);
   switchBar = !switchBar;
@@ -37,20 +35,6 @@ function change(x){
   }
 }
 
-// Code if to warp the onclick will have problem !!!! don't delete it until you note it!!
-// if (loopSwich === 'true'){
-//   document.querySelector("#card").onclick = function(){
-// console.log(loopSwich+" is true");
-//  loopSwich = !loopSwich;
-//   };
-// } else {
-//   document.querySelector("#card").onclick = function(){
-//     console.log(loopSwich+" is false");
-//  loopSwich = !loopSwich;
-//   };
-// }
-
-
 var eS = anime.timeline({
     // targets: '.tri',
     loop: 1,
@@ -61,11 +45,9 @@ var eS = anime.timeline({
   .add({
     targets: '.tri',
     background: 'rgb(0, 0, 0)',
-    translateX: eeee * 0.5 - (88 + 24),
+    translateX:  hCardWidth * 0.5 - (88 + 24),
     width: '20px',
     loopComplete: function(){
-      loopFinish = loopFinish + 1;
-      loopFinishReverse +=1;
     },
   })
   .add({
@@ -73,28 +55,23 @@ var eS = anime.timeline({
     width: '176px',
     borderRadius: '0%',
     loopComplete:  function(){
-      loopFinish = loopFinish + 1;
-      loopFinishReverse +=1;
     },
   })
   .add({
     targets: '.triGap',
     background: 'rgb(255, 255, 255)',
     easing: 'easeInQuad',
-    width: function() {
-      if (ranDom() > 0) return 20;
+    width: function(el, i, l) {
+      var x = ranDom();
+      console.log(x);
+      if (x == 0)
+      return 20;
     },
-    height: function() {
-      if (ranDom() > 0) return 20;
+    height: function(el, i, l) {
+          return 20;
     },
     duration: 200,
     loopComplete:  function(){
-      loopFinish = loopFinish + 1;
-      loopFinishReverse +=1;
-      console.log('loopFinish is: '+loopFinish);
-
-
     },
 
   });
-// document.querySelector("#card").onclick = eS.play;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 // jshint esversion:6
 // Prevent the "not(:hover)" from executing in refreshing the page
-  $("#card").mouseover(function(){
-    $("#card").addClass("homeCard");
-    console.log("Hey! have good day my friend!");
+  $("#HCard").mouseover(function(){
+    $("#HCard").addClass("homeCard");
   });
 
 // $(document).ready(function(){
